d408763d532613f8bb3391cd5bb77240869d9260
Wilson Chan <wilsonchan54@gmail.com>
2025-07-21 18:16:02 -0400

    Added heading to home.tsx / updated styling in app.css / deleted welcome folder

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,19 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Only GREATS Allowed" },
+    { name: "description", content: "Genuine feedback that companies cannot provide!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main>
+    <section className="bg-gray-950 main_section">
+      <div className="page_heading">
+        <h1 className="text_gradient">Track your personal accomplishments through Resume/CV</h1>
+        <h2>There's always more to improve on and it never ends</h2>
+      </div>
+    </section>
+  </main>
 }
