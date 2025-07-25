269fd6c0855982ebe7c60301406e5fb8f9aba475
Wilson Chan <wilsonchan54@gmail.com>
2025-07-25 18:58:28 -0400

    Created ScoreCircle component with code snippet to rendered in ResumeCard component / updated app.css

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,19 +1,20 @@
 import { Link } from "react-router"
-
-
+import ScoreCircle from "./ScoreCircle"
 
 const ResumeCard = ({resume} : { resume:Resume }) => {
   return (
     <div className="bg-blue-800">
       <Link to={`/resume/${resume.id}`} className="resume_card animate-in fade-in duration-300">
+      <div className="resume_card_header">        
         <div className="flex flex-col gap-4">
-          <h2 className="!text-black font-bold break-words">
-            {resume.companyName}
-          </h2>
-          <h3 className="text-red-600 text-lg break-words">
-            {resume.jobTitle}
-          </h3>
+          <h2 className="!text-black font-bold break-words">{resume.companyName}</h2>
+          <h3 className="text-red-600 text-lg break-words">{resume.jobTitle}</h3>
         </div>
+
+        <div className="shrink-0">
+          <ScoreCircle score={resume.feedback.overallScore} />
+        </div>
+      </div>
       </Link>
     </div>
   )
