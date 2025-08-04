5bfde0edf78be4df64e159c94e326c9b929a488e
Wilson Chan <wilsonchan54@gmail.com>
2025-08-03 22:56:06 -0400

    Added remove button and imported lib utils.ts formatSize to display estimated file size in FileUploader component / updated app.css

--- app/components/FileUploader.tsx
+++ app/components/FileUploader.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useCallback } from 'react'
 import {useDropzone} from 'react-dropzone'
+import { formatSize } from '~/lib/utils';
 
 interface FileUploaderProps{
   onFileSelect?: (file: File | null) => void;
@@ -7,6 +8,7 @@ interface FileUploaderProps{
 
 const FileUploader = ({ onFileSelect } : FileUploaderProps) => {
   const uploadIcon = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXVwbG9hZC1pY29uIGx1Y2lkZS11cGxvYWQiPjxwYXRoIGQ9Ik0xMiAzdjEyIi8+PHBhdGggZD0ibTE3IDgtNS01LTUgNSIvPjxwYXRoIGQ9Ik0yMSAxNXY0YTIgMiAwIDAgMS0yIDJINWEyIDIgMCAwIDEtMi0ydi00Ii8+PC9zdmc+`
+  const xIcon = `data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9ImN1cnJlbnRDb2xvciIgc3Ryb2tlLXdpZHRoPSIyIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGNsYXNzPSJsdWNpZGUgbHVjaWRlLXgtaWNvbiBsdWNpZGUteCI+PHBhdGggZD0iTTE4IDYgNiAxOCIvPjxwYXRoIGQ9Im02IDYgMTIgMTIiLz48L3N2Zz4=`
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
     // Do something with the files
@@ -31,19 +33,33 @@ const FileUploader = ({ onFileSelect } : FileUploaderProps) => {
         <input {...getInputProps} />
 
         <div className="space-y-5 cursor-pointer">
-          <div className="mx-auto w-16 h-16 flex items-center justify-center">
-            <img src={uploadIcon} alt="upload" className='size-20 animate-bounce' />
-          </div>
 
           {file ? (
-            <div className='flex items-center space-x-4'>
-              <img src='https://img.icons8.com/ios/200/pdf--v1.png' alt="pdf" className='size-10' />
-              <p className='text-orange-500 text-lg font-medium truncate'>
-                {file.name}
-              </p>
+            <div className="uploader_selected_file" onClick={(e) => e.stopPropagation()}>
+              <img src='https://img.icons8.com/ios/200/pdf--v1.png' alt="pdf" className='bg-green-500 size-10 rounded-xl p-1' />
+              <div className='flex items-center space-x-4'>
+                <div className="">
+                  <p className='text-orange-500 text-sm font-medium truncate max-w-xs'>
+                    {file.name}
+                  </p>
+                  <p className='text-red-600 text-sm'>
+                    {formatSize(file.size)}
+                  </p>
+                </div>
+              </div>
+
+              <button className='p-2 cursor-pointer' onClick={(e) => {
+                onFileSelect?.(null)
+              }}>
+                <img src={xIcon} alt="remove" className='w-4 h-4' />
+              </button>
             </div>
           ): (
             <div>
+              <div className="mx-auto w-16 h-16 flex items-center justify-center mb-4">
+                <img src={uploadIcon} alt="upload" className='size-20 animate-bounce' />
+              </div>
+
               <p className='text-pink-600 text-lg'>
                 <span className='font-semibold'>
                   Click to Upload
