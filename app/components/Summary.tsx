ded484aa6767a271c038f31d41cf12e438ebd9fe
Wilson Chan <wilsonchan54@gmail.com>
2025-08-14 23:51:51 -0400

    Created ATS component with code snippet

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScoreBadge from './ScoreBadge';
 
 const Category = ({ title, score } : {title:string, score:number}) => {
   const textColor = score > 70 ? 'text-green-600'
@@ -10,6 +11,7 @@ const Category = ({ title, score } : {title:string, score:number}) => {
       <div className="category">
         <div className="flex flex-row gap-2 items-center justify-center">
           <p className='text-2xl'>{title}</p>
+          <ScoreBadge score={score} />
         </div>
         <p className="text-2xl">
           <span className={textColor}>{score}</span>
