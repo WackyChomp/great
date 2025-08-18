de9a418033710b3f3f2c5d7745f21c9ba3150874
Wilson Chan <wilsonchan54@gmail.com>
2025-08-17 23:26:39 -0400

    Destructured type Resume from index.d.ts in ResumeCard component / Replace mock resume data with real data in page home.tsx (WIP)

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,28 +1,53 @@
 import { Link } from "react-router"
+import { useEffect, useState } from "react"
+import { usePuterStore } from "~/lib/puter"
+
 import ScoreCircle from "./ScoreCircle"
 
-const ResumeCard = ({resume} : { resume:Resume }) => {
+const ResumeCard = ({resume:{ companyName, id, jobTitle, feedback, imagePath }} : { resume:Resume }) => {
+  
+  const { fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState('')
+
+  useEffect(() => {
+    const loadResume = async () => {
+      const blob = await fs.read(imagePath)
+      if(!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    }
+  
+    loadResume();
+    
+  }, [imagePath])
+
   return (
     <div className="bg-blue-800">
-      <Link to={`/resume/${resume.id}`} className="resume_card animate-in fade-in duration-300">
+      <Link to={`/resume/${id}`} className="resume_card animate-in fade-in duration-300">
       <div className="resume_card_header">        
         <div className="flex flex-col gap-4">
-          <h2 className="!text-black font-bold break-words">{resume.companyName}</h2>
-          <h3 className="text-red-600 text-lg break-words">{resume.jobTitle}</h3>
+          {/* Conditionals to render */}
+          {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
+          {jobTitle && <h3 className="text-red-600 text-lg break-words">{jobTitle}</h3>}
+          {!companyName && !jobTitle && <h2 className="!text-red-600 font-bold">Resume</h2>}
         </div>
 
         <div className="shrink-0">
-          <ScoreCircle score={resume.feedback.overallScore} />
+          <ScoreCircle score={feedback.overallScore} />
         </div>
       </div>
 
-      <div className="gradient_border animate-in fade-in duration-1000">
-        <div className="w-full h-full">
-          <img src={resume.imagePath} alt='winning_resume' 
-            className="w-full h-[370px] max-sm:h-[200px] object-cover"
-          />
+      {/* only displays image if there's a ResumeUrl */}
+      {resumeUrl && (
+        <div className="gradient_border animate-in fade-in duration-1000">
+          <div className="w-full h-full">
+            <img src={imagePath} alt='winning_resume' 
+              className="w-full h-[370px] max-sm:h-[200px] object-cover"
+            />
+          </div>
         </div>
-      </div>
+      )}
+
       </Link>
     </div>
   )
